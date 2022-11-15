9aa8f30666f7b5781f3f2c00b5c9685fcf52ffc7
AdonayAlvarado <AdonayAlvarado>
2022-11-14 22:51:37 -0600

    Version 1.2

--- app/src/main/java/com/example/calculadorapolacainversaapp/MainActivity.java
+++ app/src/main/java/com/example/calculadorapolacainversaapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-EditText num1,num2,signo;//hol
+EditText num1,num2,signo;//holi
 TextView res;
 double ope;
     @Override
